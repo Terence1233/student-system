27a71171509f5b9b5d64465cb83e8a10a8bf8835
zhaobaoqiang <baoqiang.zhao@sdbean.com>
2021-01-30 14:14:32 +0800

    init project

---
+++ src/main/java/com/braisedpanda/shirotest/controller/CommonController.java
@@ -0,0 +1,17 @@
+package com.braisedpanda.shirotest.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0
+ * @Description:
+ * @Author: 赵宝强
+ * @CreateTime: 2021/1/30 13:16
+ */
+@RestController
+@Slf4j
+public class CommonController {
+
+}
---
+++ src/main/java/com/braisedpanda/shirotest/model/vo/BulletinBoardRespVo.java
@@ -0,0 +1,15 @@
+package com.braisedpanda.shirotest.model.vo;
+
+import lombok.Data;
+
+/**
+ * @version 1.0
+ * @Description:
+ * @Author: 赵宝强
+ * @CreateTime: 2021/1/30 13:18
+ */
+@Data
+public class BulletinBoardRespVo {
+
+
+}
